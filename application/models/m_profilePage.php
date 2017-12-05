bfff2ccab73af839129e0df094476623472644ee
faizinahsan <faizin110998@gmail.com>
2017-12-05 20:13:07 +0700

    nambahin wishlist

--- application/models/m_profilePage.php
+++ application/models/m_profilePage.php
@@ -91,7 +91,10 @@ class m_profilePage extends CI_Model
 		$this->db->from('paper');
 		$this->db->join('user_has_paper', 'paper.idPaper = user_has_paper.Paper_idPaper');
 		$this->db->join('user', 'paper.File_User_idUser = user.idUser');
-		$this->db->where('user_has_paper.User_IdUser',$idUser);
+		$this->db->where([
+			'user_has_paper.User_IdUser'=>$idUser,
+			'user_has_paper.isWishlist'=>1
+		]);
 		$query = $this->db->get();
 		return $query->result_array();
 	}
